76e80315d4c345d5b590d4d4f59192018d1008a3
Chi Hong Edmund Chan <pokedev8@gmail.com>
2018-01-05 21:43:30 -0800

    #02 - Mouse clicking to detect candles when mouse down held done.

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	RaycastHit2D m_Ray;
+	float RAY_DISTANCE = 1f;
+
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 localPos = new Vector2(worldPos.x, worldPos.y);
+			m_Ray = Physics2D.Raycast(localPos, Vector2.down, RAY_DISTANCE);
+			if (m_Ray.collider)
+			{
+				Debug.Log(m_Ray.collider.tag);
+				Debug.Log(m_Ray.transform.position);
+			}
+		}
+	}
+}
