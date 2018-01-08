48feb03e19fb2b5df69c46051b68de8ceac32cc7
Chi Hong Edmund Chan <pokedev8@gmail.com>
2018-01-07 17:29:58 -0800

    #06 - Quick commit on tidying code, in the future, will have another condition later that will stop play (when rules broken)

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -13,11 +13,10 @@ public class PlayerController : MonoBehaviour
 	{
 		if (Input.GetMouseButton(0) && !m_InPlay)
 		{
-			m_InPlay = true;
 			Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 localPos = new Vector2(worldPos.x, worldPos.y);
-			m_Ray = Physics2D.Raycast(localPos, Vector2.down, RAY_DISTANCE);
-			if (m_Ray.collider && m_InPlay)
+			m_Ray = Physics2D.Raycast(localPos, Vector2.zero, RAY_DISTANCE);
+			if (m_Ray.collider)
 			{
 				TileScript tile = m_Ray.collider.GetComponent<TileScript>();
 				if (!tile.ReturnLit())
@@ -27,6 +26,9 @@ public class PlayerController : MonoBehaviour
 					tile.Light();
 				}
 			}
+
+			//TODO later
+			//if(condition where rules are broken (touching candle too far from last point)) { m_InPlay = true;}
 		}
 		else if (Input.GetMouseButtonUp(0))
 		{
