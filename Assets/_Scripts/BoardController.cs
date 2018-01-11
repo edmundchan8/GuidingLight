b997cefb50e70eb1b972fd179649c5c574f472b4
Chi Hong Edmund Chan <pokedev8@gmail.com>
2018-01-10 22:57:39 -0800

    #13 - Scriptable object for row data made so that each row for a level is made with scriptable objects.

--- Assets/_Scripts/BoardController.cs
+++ Assets/_Scripts/BoardController.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 public class BoardController : MonoBehaviour 
 {
-	List<int> m_XPosList = new List<int>();
-	List<int> m_YPosList = new List<int>();
+	Dictionary<int, int> m_XPosDictionary = new Dictionary<int, int>();
+	Dictionary<int, int> m_YPosDictionary = new Dictionary<int, int>();
 
-	public void AddXList (int element)
+	public RowData[] m_RowData;
+
+	void Start()
 	{
-		m_XPosList.Add(element);
+		CreateBoard();
 	}
 
-	public void AddYList (int element)
+	public void CreateBoard()
 	{
-		m_YPosList.Add(element);
+		for (int y = 0; y < m_RowData.Length; y++)
+		{
+			for (int x = 0; x < m_RowData[y].m_Tiles.Length; x++)
+			{
+				GameObject tile = Instantiate(m_RowData[y].m_Tiles[x], transform.position, transform.rotation) as GameObject;
+				Vector2 pos = new Vector2(tile.transform.position.x, tile.transform.position.y);
+				pos.x = x;
+				pos.y = y;
+				tile.transform.position = pos;
+			}
+		}
+	}
+
+	public void AddXDictionary (int element, int value)
+	{
+		m_XPosDictionary.Add(element, value);
+	}
+
+	public void AddYDictionary (int element, int value)
+	{
+		m_YPosDictionary.Add(element, value);
 	}
 
 	public void ClearXList()
 	{
-		m_XPosList.Clear();
+		m_XPosDictionary.Clear();
 	}
 
 	public void ClearYList()
 	{
-		m_YPosList.Clear();
+		m_YPosDictionary.Clear();
 	}
 }
