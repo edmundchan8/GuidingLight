ff968307fed3d2ed6caaf7c7dfc84a44e1fa4a50
Chi Hong Edmund Chan <pokedev8@gmail.com>
2018-01-16 22:42:06 -0800

    #20 - Redid numbers, slightly smaller.  Camera also takes the max rows and columns to place board at the centre each time

---
+++ Assets/FollowBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBoard : MonoBehaviour 
+{
+	public WinLoseData m_WinLoseData;
+
+	float IncrementCameraPosValue = 0.5f;
+
+	void Start()
+	{
+		float xPos = (m_WinLoseData.m_MaxLineColumn.Length - 1) * IncrementCameraPosValue;
+		float yPos = (m_WinLoseData.m_MaxLineRow.Length - 1) * IncrementCameraPosValue;
+
+		transform.position = new Vector3(xPos, yPos, transform.position.z);
+	}
+
+}
